d95535d019177ee1fbecefd5014982c28582878d
MayaSasson <mayokit22@gmail.com>
2020-03-23 23:16:12 +0200

    feat(basis)

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      fixed: false,
+    }
+  }
+}
+</script>
