dd06a741189070e1d782abe2fb7988f01889ab58
MayaSasson <mayokit22@gmail.com>
2020-03-25 02:35:26 +0200

    now the assistant font works

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ module.exports = {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: 'NewSpectrum',
+    title: process.env.npm_package_name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Assistant|Libre+Baskerville&display=swap%27' }
     ]
   },
   /*
@@ -25,6 +25,7 @@ module.exports = {
   ** Global CSS
   */
   css: [
+    // '~/assets/fonts.css'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -49,6 +50,7 @@ module.exports = {
   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
     theme: {
       primary: colors.blue.darken2,
       accent: colors.grey.darken3,
